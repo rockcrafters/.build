46f0357ea69477724b03f1f242c5a231d3fc7c39
Cristovao Cordeiro <cristovao.cordeiro@canonical.com>
2022-08-19 15:03:29 +0200

    feat: add release-tags

--- create-git-tags.sh
+++ create-git-tags.sh
@@ -1,17 +1,17 @@
 #!/bin/bash -eux
 
-# In theory it is possible to do this via the GitHub API
-# https://docs.github.com/en/rest/git/tags
-# But for some reason it is failing for "verification" purposes
-# (see the create-git-tags.py if trying to solve the issue)
-
-# Using the CLI instead
+# Using the CLI
 
 token="$1"
 marked_for_publishing="$2"
 branch="$3"
 rock_repository="$4"
 rock_commit="$5"
+track=${6:-$(echo $branch | cut -d '/' -f 2)}
+risk="$7"
+# use skip_build_tag when only publishing the release tag
+# i.e., if channel/<track>/<rock>/<version>/<base>/<revision> already exists
+skip_build_tag="$8"
 
 # Move to rock repo and create tag
 pushd $ROCK_PROJECT_CHECKOUT_LOCATION
@@ -20,7 +20,8 @@ git config user.name "ROCKS Team"
 git remote add upstream-rock https://rocks-team:${token}@github.com/${rock_repository}
 
 tag_prefix=$(echo $branch | awk -F"/" '{print $1"/"$2}')
-all_tags=()
+build_tags=()
+release_tags=()
 while IFS= read -r rock
 do 
     rock_name="$(echo $rock | cut -d ',' -f 2)"
@@ -29,15 +30,31 @@ do
     rock_revision="$(echo $rock | cut -d ',' -f 6)"
     oci_tags="$(echo $rock | cut -d ',' -f 7)"
 
-    tag_name="${tag_prefix}/${rock_version}/${rock_name}/${rock_base}/${rock_revision}"
+    if [ "$skip_build_tag" != "1" ] && [ "$skip_build_tag" != "true" ]
+    then
+        tag_name="${tag_prefix}/${rock_name}/${rock_version}/${rock_base}/${rock_revision}"
 
-    git tag -a \
-        -m "New revision ${rock_revision} of ${rock_name} ${rock_version} created (OCI tags: ${oci_tags})" \
-        $tag_name $rock_commit
+        git tag -a \
+            -m "New revision ${rock_revision} of ${rock_name} ${rock_version} created (OCI tags: ${oci_tags})" \
+            $tag_name $rock_commit
 
-    git push upstream-rock $tag_name
-    all_tags+=( $tag_name )
+        git push upstream-rock $tag_name
+        build_tags+=( $tag_name )
+    fi 
+
+    if [ ! -z $risk ]
+    then
+        # There's a risk in the channel branch, so let's release the ROCK
+        release_tag="release/${rock_name}/${rock_revision}/${track}/${risk}"
+        git tag -a \
+            -m "Releasing revision ${rock_revision} of ${rock_name} ${rock_version} to ${track}/${risk}" \
+            $release_tag $rock_commit
+
+        git push upstream-rock $release_tag
+        release_tags+=( $release_tag )
+    fi
 done < "${marked_for_publishing}"
 popd
 
-echo "::set-output name=git-tags::${all_tags[@]}"
+echo "::set-output name=build-tags::${build_tags[@]}"
+echo "::set-output name=release-tags::${build_tags[@]}"
