80d046d55b355ec95298dcbc34220f152690fff1
Cristovao Cordeiro <cristovao.cordeiro@canonical.com>
2022-08-22 13:12:24 +0200

    fix: variable enclosing

--- oci-re-tag-and-release.sh
+++ oci-re-tag-and-release.sh
@@ -6,9 +6,9 @@ token="$1"
 new_oci_tags=""
 for risk in $RELEASE_FOR_RISKS
 do
-    new_oci_tags="${new_oci_tags} $TRACK-$BASE_${risk}_$REVISION"
-    new_oci_tags="${new_oci_tags} $TRACK-$BASE_${risk}"
-    new_oci_tags="${new_oci_tags} $TRACK_${risk}"
+    new_oci_tags="${new_oci_tags} ${TRACK}-${BASE}_${risk}_$REVISION"
+    new_oci_tags="${new_oci_tags} ${TRACK}-${BASE}_${risk}"
+    new_oci_tags="${new_oci_tags} ${TRACK}_${risk}"
     new_oci_tags="${new_oci_tags} ${risk}"
     if [ "${risk}" == "stable" ]
     then 
@@ -51,7 +51,7 @@ curl \
   -X POST \
   -H "Accept: application/vnd.github+json" \ 
   -H "Authorization: token $token" \
-  https://api.github.com/repos/$ROCK_REPO/releases \
+  https://api.github.com/repos/${ROCK_REPO}/releases \
   -d @gh_release.txt
 
 rm gh_release.txt || true
